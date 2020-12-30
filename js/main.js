47af746fd02528188828165fe940c8f39d4b0e33
shubhamkarande <76203740+shubhamkarande@users.noreply.github.com>
2020-12-30 16:24:43 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if(this.hash !== '') {
+        
+        const hash = this.hash;
+        
+        $('html, body').animate(
+        {
+            scrollTop: $(hash).offset().top -80
+        },
+        800
+        );
+    }
+});
