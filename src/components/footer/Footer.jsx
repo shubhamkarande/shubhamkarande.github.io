8daf6a4208535f8e02d6e00f3c783ea2a4605b80
Shubham Karande <shubhamkarande94@gmail.com>
2025-08-04 12:04:53 +0530

    Replaced social icons with new ones

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,75 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="footer__container container">
-            <h1 className="footer__title">Shubham</h1>
+      <div className="footer__container container">
+        <h1 className="footer__title">Shubham</h1>
 
-            <ul className="footer__list">
-                <li>
-                    <a href="#about" className="footer__link">About</a>
-                </li>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
 
-                <li>
-                    <a href="#portfolio" className="footer__link">Projects</a>
-                </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
 
-                <li>
-                    <a href="#services" className="footer__link">Services</a>
-                </li>
-            </ul>
+          <li>
+            <a href="#services" className="footer__link">
+              Services
+            </a>
+          </li>
+        </ul>
 
-            <div className="footer__social">
-        <a href="https://www.linkedin.com/in/karandeshubham/" className="footer__social-link" target="_blank"><i className="bx bxl-linkedin"></i></a>
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com/in/karandeshubham/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-linkedin"></i>
+          </a>
 
-        <a href="https://www.instagram.com/_stfu.shubham_/" className="footer__social-link" target="_blank"><i className="bx bxl-instagram"></i></a>
+          <a
+            href="https://github.com/shubhamkarande"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-github"></i>
+          </a>
 
-        <a href="https://x.com/stfu_shubham09" className="footer__social-link" target="_blank"><i className="bx bxl-twitter"></i></a>
-            </div>
-
-            <span className="footer__copy">&#169; 2025 Shubham Karande. All rights reserved.</span>
+          <a
+            href="https://leetcode.com/u/stfushubham/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              style={{ display: "block" }}
+            >
+              <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z" />
+            </svg>
+          </a>
         </div>
+
+        <span className="footer__copy">
+          &#169; 2025 Shubham Karande. All rights reserved.
+        </span>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
